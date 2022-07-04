1c75ad57246260aff1bd6821a4f607fa648c66f8
kmfoysal <faisal.cri@gmail.com>
2022-07-04 17:40:08 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Import stylesheets
 import './style.css';
 
+console.log('Hello')
+
+
+const syncWithCallBack = () =>{
+  for(let i=0; i<10; i++){
+    console.log('looping: \t', i, '\t', new Date() )
+  }
+}
+
+syncWithCallBack();
+
+console.log('Done')
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Async Js</h1>`;
